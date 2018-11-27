cab09e53e6c864a4018f6e7fdc5638a1bc9473a9
Justin Brevik <jlbrevik@gmail.com>
2018-11-26 19:09:08 -0700

    First Commit

---
+++ test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from flask import request
+from flask import json
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+from pythonosc import udp_client
+
+@app.route("/play", methods=['POST'])
+@cross_origin()
+def sound_play():
+	board = request.form['board']
+	id = request.form['id']
+
+	board_location = '/castersoundboard/board/' + str(board) + '/player/' + str(id) + '/modify/play_state/play'
+	client = udp_client.SimpleUDPClient('127.0.0.1', 5051)
+	client.send_message(board_location, 1)
+	return ""
+
+@app.route("/stop", methods=['POST'])
+@cross_origin()
+def sound_play():
+	board = request.form['board']
+	id = request.form['id']
+
+	board_location = '/castersoundboard/board/' + str(board) + '/player/' + str(id) + '/modify/play_state/play'
+	client = udp_client.SimpleUDPClient('127.0.0.1', 5051)
+	client.send_message(board_location, 1)
+	return ""
+
+
+
+if __name__ == '__main__':
+	app.run(host = '0.0.0.0',port=5000)
+
