6db68c2b3b4b2bf9a490b8392a8c1c9f68f41ab7
Justin Brevik <jlbrevik@gmail.com>
2018-12-22 16:45:23 -0700

    Adding gunicorn for flask hosting

--- soundboard.py
+++ soundboard.py
@@ -11,7 +11,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 from pythonosc import udp_client
 
-@app.route("/", methods=['POST'])
+@app.route("/playback", methods=['POST'])
 @cross_origin()
 def sound_play():
 	board = request.form['board']
@@ -22,19 +22,8 @@ def sound_play():
 	client.send_message(board_location, 1)
 	return ""
 
-# @app.route("/stop", methods=['POST'])
-# @cross_origin()
-# def sound_play():
-# 	board = request.form['board']
-# 	id = request.form['id']
-
-# 	board_location = '/castersoundboard/board/' + str(board) + '/player/' + str(id) + '/modify/play_state/play'
-# 	client = udp_client.SimpleUDPClient('127.0.0.1', 5051)
-# 	client.send_message(board_location, 1)
-# 	return ""
 
 
-
-if __name__ == '__main__':
-	app.run(host = '0.0.0.0',port=5000)
+#if __name__ == '__main__':
+#	app.run(host = '0.0.0.0',port=5000)
 
